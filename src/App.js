51f6f587d94e06f73ff3d3940af0aea0e90ddec8
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-23 22:32:02 +0530

    add images

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import Weather from "./component/Weather";
 function App() {
-  return "hello";
+  return <Weather />;
 }
 
 export default App;
---
+++ src/component/weather.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@material-ui/core";
+
+const Weather = () => {
+  return (
+    <Box>
+      <Box></Box>
+      <Box></Box>
+    </Box>
+  );
+};
+
+export default Weather;
